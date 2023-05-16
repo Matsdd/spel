d727eae17a9e207dca1843425c9a545c21d416ed
Matsdd <Matsdoornhein04@gmail.com>
2023-05-16 22:34:18 +0200

    try again

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,8 +2,6 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 import ExploderImage from '../sprites/robots/ExploderI.png'
 import BackgroundImage from '../images/Background.png'
 
-
-
 const Resources = {
     Exploder: new ImageSource(ExploderImage),
     Background: new ImageSource(BackgroundImage)
