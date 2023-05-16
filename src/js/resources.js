d40fd3f57f07f048889f75105653002e0aae1220
Matsdd <Matsdoornhein04@gmail.com>
2023-05-16 22:27:42 +0200

    -shrek

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,15 +1,13 @@
 import { ImageSource, Sound, Resource, Loader } from 'excalibur'
-import shrekImage from '../images/shrek.png'
 import ExploderImage from '../sprites/robots/ExploderI.png'
 import BackgroundImage from '../images/Background.png'
 
 
 
 const Resources = {
-    Shrek: new ImageSource(shrekImage),
     Exploder: new ImageSource(ExploderImage),
     Background: new ImageSource(BackgroundImage)
 }
-const ResourceLoader = new Loader([Resources.Shrek, Resources.Exploder, Resources.Background])
+const ResourceLoader = new Loader([Resources.Exploder, Resources.Background])
 
 export { Resources, ResourceLoader }
